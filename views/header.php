6ef76e6b5cde360f946c68d01b92fc51f351de48
Plesca Dragos <plescadragoss@yahoo.com>
2015-07-13 17:41:50 +0300

    Added simple views

---
+++ views/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Fii cel de-al patrulea jurat X Factor!">
+    <meta property="og:url" content="http://liked.ro/apps/jurat-xfactor/"/>
+    <meta property="og:title" content="Devino Jurat X Factor!"/>
+    <meta property="og:description" content="X Factor! Show la puterea X!"/>
+    <!-- <meta property="og:image" content="http://liked.ro/apps/hai-simona/img/appicon.jpg"/> -->
+    <title>Devino Jurat X Factor!</title>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/main-style.css">
+</head>
+<body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div id="content">
